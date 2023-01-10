8a4568edefff377f9d5a69a767b70cd7a59f1ab1
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 00:45:21 +0545

    Filtering the path

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form method = 'POST' action='/product'> <input type = 'text' name = 'title' > <button type = 'submit' >send </button> </form>"
+    "<form method = 'POST' action='/admin/add-product'> <input type = 'text' name = 'title' > <button type = 'submit' >send </button> </form>"
   );
 });
 
-router.post("/product", (req, res) => {
+router.post("/add-product", (req, res) => {
+  // "/add-product" can be used and the methods are different in the middleware. ie GET and POST
   console.log(req.body);
   res.redirect("/");
 });
