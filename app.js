1d3b054f92c2d7d944e2001a921c26f67a3ea6bb
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 00:00:13 +0545

    How middleware works

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 app.use((req, res, next) => {
   // runs for every incoming requests
   console.log("In the middleware");
-  next();
+  next(); // Allows the request to continue to the next middleware in line
 });
 
- app.use((req, res, next) => {
+app.use((req, res, next) => {
   console.log("In the next middleware");
+  res.send("<h1>Hello from express!!</h1>"); //send method provided by express.js attaches all the headers and requires things
 });
 
 const server = http.createServer(app);
