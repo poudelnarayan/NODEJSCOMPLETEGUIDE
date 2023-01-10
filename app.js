f6a3c7384fb909b8fc8b8250ac27a1c61a63b31c
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 00:59:46 +0545

    Returning a 404 page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const bodyParser = require("body-parser");
@@ -13,7 +15,7 @@ app.use("/admin", adminRoutes); // only routes starting with /admin will go into
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send("<h1>Page not found </h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
