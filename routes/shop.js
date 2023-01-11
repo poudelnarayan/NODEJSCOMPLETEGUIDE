1a0b5353f8badcc8c84de64a9ee7d00bb71104bb
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 13:32:46 +0545

    converting our project to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,12 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   // the render method somplu allows to pass in data that should be added into our view
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
