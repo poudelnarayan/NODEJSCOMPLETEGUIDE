734d6fac55d4aae0923f6d1edee233350b1671ca
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 17:53:42 +0545

    Displaying product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,6 @@ exports.postAddProduct = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
