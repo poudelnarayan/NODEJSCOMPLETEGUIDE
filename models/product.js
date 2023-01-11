07f702765f360a693d98f6194de096a874325716
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 14:54:31 +0545

    Storing data in Files via Models

--- models/product.js
+++ models/product.js
@@ -1,15 +1,33 @@
-const products = [];
-
+const fs = require("fs");
+const path = require("path");
+const absPath = require("../util/path");
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(absPath, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this); // to use 'this' we must use arrow function otherwise 'this' will loose its context
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(absPath, "data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
