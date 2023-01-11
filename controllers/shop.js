c17248a81df39ffddd1d5af0cb513ca9d78e22df
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 18:07:19 +0545

    Adding orders section

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,3 +33,10 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: "Chechout",
   });
 };
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: " Your Orders",
+  });
+};
