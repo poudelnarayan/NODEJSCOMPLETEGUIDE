c3f9fb784ae9bff114898bbbeef4ba016973ab10
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-11 13:18:52 +0545

    Working with handlebars

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const path = require("path");
 
 const express = require("express");
-
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views"); // its the default setting though . we dont need to mention here
+app.engine("hbs", expressHbs()); // we didnt do this in pug because that was a built in template engine
+
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
