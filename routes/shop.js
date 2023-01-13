c263cf3ed75c9265c7e714eab8548fa6d7edf891
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-13 14:58:38 +0545

    extracting dynamic params

--- routes/shop.js
+++ routes/shop.js
@@ -12,6 +12,8 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+router.get("/products/:productId", shopController.getProductDetails);
+
 router.get("/cart", shopController.getCart);
 
 router.get("/checkout", shopController.getCheckout);
