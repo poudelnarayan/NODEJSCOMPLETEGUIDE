7582b38c53218b71b183fc538f6a822e010f3a8c
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-16 13:06:53 +0545

    Creating New User

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -15,6 +16,16 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("63c4faa8903d289648a5dc88")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoDb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+const ObjectId = mongoDb.ObjectId;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    db.collection("user").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: new ObjectId(userId) });
+  }
+}
+
+module.exports = User;
